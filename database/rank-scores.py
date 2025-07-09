9c54515c3938f90a2c0337f1384b0a830c981b8f
anqihuang01 <149406909+anqihuang01@users.noreply.github.com>
2025-07-09 08:42:18 +0800

    LC-rank-scores [Runtime: 291 ms; Memory: 68.3 MB]

--- database/rank-scores.py
+++ database/rank-scores.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    Scores['rank'] = Scores['score'].rank(method ='dense',ascending=False)
-    return Scores[['score','rank']].sort_values('score',ascending=False)    
+    scores['rank'] = scores['score'].rank(method ='dense',ascending=False)
+    return scores[['score','rank']].sort_values('score',ascending=False)    
